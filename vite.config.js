0be57fc71231d20d75be21ead7a7e961bf88af76
Gopal Dawar <gopaldawar45@gmail.com>
2025-05-23 09:10:42 +0530

    Update for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+<<<<<<< HEAD
   base: '/react_clothe_shop_website/',
+=======
+  base: '/react_clothe_shop_website/', // 👈 Add your repo name here
+>>>>>>> 31b7599 (Update for GitHub Pages deployment)
   plugins: [react()],
 })
